37565d919ce92c038281a430e5fedf2c2f4f6d51
Rahul Choudhury <rchoudhury63@gmail.com>
2025-11-11 14:32:40 +0530

    wip: shell with local state management

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,73 @@
-import Image from "next/image";
+"use client";
+
+import * as React from "react";
+import { Input } from "@base-ui-components/react";
+
+type Bookmark = {
+  link: string;
+  timeStamp: string;
+};
 
 export default function Home() {
+  const [bookmarks, setBookmarks] = React.useState<Bookmark[]>([]);
+  const searchInputRef = React.useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = Object.fromEntries(new FormData(e.currentTarget));
+    setBookmarks((prev) => [
+      ...prev,
+      {
+        link: String(form.search),
+        timeStamp: new Date().toISOString(),
+      },
+    ]);
+  };
+
+  React.useEffect(() => {
+    const handleSearch = (e: KeyboardEvent) => {
+      if (e.target === searchInputRef.current) {
+        return;
+      }
+
+      if (e.key === "/") {
+        e.preventDefault();
+        searchInputRef.current?.focus();
+      }
+    };
+
+    document.addEventListener("keydown", handleSearch);
+    return () => document.removeEventListener("keydown", handleSearch);
+  }, []);
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <div className="w-full max-w-4xl mx-auto p-8 space-y-4">
+      <form onSubmit={handleSubmit}>
+        <Input
+          ref={searchInputRef}
+          name="search"
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              e.preventDefault();
+              e.currentTarget.form?.requestSubmit();
+              e.currentTarget.form?.reset();
+            }
+          }}
+          placeholder="Search or paste URL (Press '/' to focus)"
+          className="w-full px-4 py-2 text-base border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </form>
+
+      <ul className="space-y-2 text-sm">
+        {bookmarks.map(({ link, timeStamp }) => (
+          <li key={timeStamp} className="flex justify-between">
+            {link}
+            <p className="text-gray-500">
+              {new Date(timeStamp).toLocaleDateString()}
+            </p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
