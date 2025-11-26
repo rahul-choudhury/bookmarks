072a8ae484292e1f76a0dfa0e93355911f72dddc
Rahul Choudhury <rchoudhury63@gmail.com>
2025-11-26 14:35:08 +0530

    feat: add <esc><esc> input clear shortcut for search bar

--- components/search-bar.tsx
+++ components/search-bar.tsx
@@ -7,6 +7,8 @@ import { useBookmarks } from "@/components/providers/bookmarks-provider";
 
 export function SearchBar() {
   const searchInputRef = React.useRef<HTMLInputElement>(null);
+  const lastEscPressRef = React.useRef(0);
+
   const { bookmarks, searchTerm, setSearchTerm, addOptimisticBookmark } =
     useBookmarks();
 
@@ -14,6 +16,41 @@ export function SearchBar() {
     success: false,
     message: "",
   });
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Escape") {
+      const now = Date.now();
+      if (now - lastEscPressRef.current < 300) {
+        setSearchTerm("");
+      }
+      lastEscPressRef.current = now;
+      return;
+    }
+
+    if (e.key === "Enter") {
+      e.preventDefault();
+
+      if (!isUrl(searchTerm)) return;
+      if (bookmarks.length > 0) return; // early return if duplicate link is found
+
+      setSearchTerm("");
+
+      addOptimisticBookmark({
+        id: crypto.randomUUID(),
+        userId: "temp", // TODO: should i leave temp or add the actual id from session?
+        url: searchTerm.startsWith("http")
+          ? searchTerm
+          : `https://${searchTerm}`,
+        title: searchTerm,
+        favicon: null,
+        timeStamp: new Date(),
+      });
+
+      React.startTransition(() => {
+        action(searchTerm);
+      });
+    }
+  };
 
   React.useEffect(() => {
     const handleSearch = (e: KeyboardEvent) => {
@@ -38,31 +75,7 @@ export function SearchBar() {
         name="search"
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
-        onKeyDown={(e) => {
-          if (e.key === "Enter") {
-            e.preventDefault();
-
-            if (!isUrl(searchTerm)) return;
-            if (bookmarks.length > 0) return; // early return if duplicate link is found
-
-            setSearchTerm("");
-
-            addOptimisticBookmark({
-              id: crypto.randomUUID(),
-              userId: "temp", // TODO: should i leave temp or add the actual id from session?
-              url: searchTerm.startsWith("http")
-                ? searchTerm
-                : `https://${searchTerm}`,
-              title: searchTerm,
-              favicon: null,
-              timeStamp: new Date(),
-            });
-
-            React.startTransition(() => {
-              action(searchTerm);
-            });
-          }
-        }}
+        onKeyDown={handleKeyDown}
         placeholder="Search or paste URL (Press '/' to focus)"
         className="w-full px-4 py-2 text-base border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-500 ring-offset-2"
       />
