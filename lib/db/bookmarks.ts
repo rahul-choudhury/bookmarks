6d52300f20ff2b5d5a56fafb4f32fb956dbfa57f
Rahul Choudhury <rchoudhury63@gmail.com>
2025-11-26 13:04:40 +0530

    fix: db level constraint for duplicate links

--- lib/db/bookmarks.ts
+++ lib/db/bookmarks.ts
@@ -1,15 +1,19 @@
 import { user } from "@/auth-schema";
-import { pgTable, timestamp, uuid, text } from "drizzle-orm/pg-core";
+import { pgTable, timestamp, uuid, text, unique } from "drizzle-orm/pg-core";
 
-export const bookmarksTable = pgTable("bookmarks", {
-  id: uuid().primaryKey().defaultRandom(),
-  url: text().notNull(),
-  title: text(),
-  favicon: text(),
-  timeStamp: timestamp().notNull().defaultNow(),
-  userId: text("user_id")
-    .notNull()
-    .references(() => user.id, { onDelete: "cascade" }),
-});
+export const bookmarksTable = pgTable(
+  "bookmarks",
+  {
+    id: uuid().primaryKey().defaultRandom(),
+    url: text().notNull(),
+    title: text(),
+    favicon: text(),
+    timeStamp: timestamp().notNull().defaultNow(),
+    userId: text("user_id")
+      .notNull()
+      .references(() => user.id, { onDelete: "cascade" }),
+  },
+  (table) => [unique().on(table.url, table.userId)],
+);
 
 export type Bookmark = typeof bookmarksTable.$inferSelect;
