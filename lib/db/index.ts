d7046ba923e707a895fc96042d0cc08dca9d764a
Rahul Choudhury <rchoudhury63@gmail.com>
2025-11-27 00:01:23 +0530

    refactor: migrate away from neon

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,6 +1,8 @@
-import { drizzle } from "drizzle-orm/neon-http";
-import { loadEnvConfig } from "@next/env";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 
-loadEnvConfig(process.cwd());
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL!,
+});
 
-export const db = drizzle(process.env.DATABASE_URL!);
+export const db = drizzle(pool);
