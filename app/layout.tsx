3a61451ee032e4696156c1215622adc114db88dd
Rahul Choudhury <rchoudhury63@gmail.com>
2025-12-09 20:42:09 +0530

    feat: og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ const jetbrainsMono = JetBrains_Mono({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://bookmarks.rchoudhury.dev"),
   title: "Bookmarks",
   description: "<kbd> focused bookmark manager",
 };
