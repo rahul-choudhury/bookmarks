f1e04908f240e2d8614cf944c0d5b6517a37c48b
Rahul Choudhury <rchoudhury63@gmail.com>
2025-12-09 20:53:56 +0530

    fix: allow opengraph

--- proxy.ts
+++ proxy.ts
@@ -23,6 +23,6 @@ export async function proxy(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|.*\\.png$|.*\\.svg$).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|opengraph-image|.*\\.png$|.*\\.svg$).*)",
   ],
 };
