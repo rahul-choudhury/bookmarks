d533eeebe508a7b10e82e6018583c40fa8c6a261
Rahul Choudhury <rchoudhury63@gmail.com>
2025-12-18 21:18:45 +0530

    feat: add gradient blur overlay

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,8 +20,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${jetbrainsMono.variable} font-mono antialiased`}>
+      <body
+        className={`${jetbrainsMono.variable} relative font-mono antialiased`}
+      >
         {children}
+        <div className="pointer-events-none fixed bottom-0 left-0 h-24 w-full bg-white/90 mask-[linear-gradient(rgba(0,0,0,0)_0%,black_100%)] blur-xs" />
       </body>
     </html>
   );
