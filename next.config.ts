9361fb3499ead2c2fb1ad06551f46a1f35f8edb6
Rahul Choudhury <rchoudhury63@gmail.com>
2025-12-24 00:54:23 +0530

    refactor: migrate from neon to PostgreSQL

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-      },
-    ],
-  },
+  output: "standalone",
 };
 
 export default nextConfig;
